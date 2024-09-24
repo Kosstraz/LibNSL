290c691bbf6895f402f9ad9396c0494a4cdb0409
Kosstraz <60485785+Kosstraz@users.noreply.github.com>
2024-09-25 01:29:02 +0200

    Update Maths.hpp

--- NSL/env/srcs/Maths.hpp
+++ NSL/env/srcs/Maths.hpp
@@ -8,14 +8,14 @@ namespace Maths
 
 inline static constexpr double	Pi = 3.14159265359;
 
-inline double	DegToRad(const double& deg)
+inline float	DegToRad(const float& deg)
 {
-	return (deg * (Maths::Pi / 180.0));
+	return (deg * (static_cast<float>(Maths::Pi) / 180.0f));
 }
 
-inline double	RadToDeg(const double& rad)
+inline float	RadToDeg(const float& rad)
 {
-	return (rad * (180.0 / Maths::Pi));
+	return (rad * (180.0f / static_cast<float>(Maths::Pi)));
 }
 
 }
